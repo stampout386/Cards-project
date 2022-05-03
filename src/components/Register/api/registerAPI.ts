5a5db5e92f1bfb4f4dfa7e639a1304f2b7eabd28
Mikhail Shestak <stampout386@gmail.com>
2022-05-03 10:56:27 +0300

    delete register Api

--- src/components/Register/api/registerAPI.ts
+++
@@ -1,8 +0,0 @@
-import {instance} from "../../../api/cardsAPI";
-
-export const registerAPI = {
-    signUp: async (payload:any) => {
-        const response = await instance.post('/auth/register', payload)
-        return response.data
-    }
-}
