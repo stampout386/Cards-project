252307d60e0706d4770a04314eea8188ae690784
Mikhail Shestak <stampout386@gmail.com>
2022-05-03 11:29:29 +0300

    draw for component Profile JSX

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import style from './Profile.module.css'
+import SuperButton from "../common/SuperButton/SuperButton";
 
 export function Profile() {
-    return <div>Profile</div>;
+    return <div className={style.container}>
+        Personal information
+        <div>
+            <img src="https://cspromogame.ru//storage/upload_images/avatars/1299.jpg" alt="" className={style.avatar}/>
+        </div>
+        <div>
+            <div className={style.description}>Nickname</div>
+            <div>Mik</div>
+        </div>
+        <div>
+            <div className={style.description}>Email</div>
+            <div>Mik@gmail.com</div>
+        </div>
+        <div className={style.buttons}>
+
+            <SuperButton className={style.button}>Log out</SuperButton>
+            <SuperButton className={style.button}>Save</SuperButton>
+        </div>
+
+    </div>;
 }
