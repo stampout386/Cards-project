7e551c7d0039b609a11bbd72d5692e435eb9d9e1
Mikhail Shestak <stampout386@gmail.com>
2022-05-03 11:54:13 +0300

    fix bag for reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,5 @@
 import {combineReducers} from "redux";
 import {errorPageReducer, testPageReducer} from "./reducers";
-import {profileReducer} from "./profileReducer";
 import {loginReducer} from "./loginReducer";
 import {newPasswordReducer} from "./newPasswordReducer";
 import {recoverPasswordReducer} from "./recoverPasswordReducer";
@@ -8,15 +7,14 @@ import {legacy_createStore as createStore} from 'redux'
 
 
 const rootReducer = combineReducers({
-    profilePage: profileReducer,
     loginPage: loginReducer,
-    newPassword: newPasswordReducer,
-    recoverPassword: recoverPasswordReducer,
-    testPage: testPageReducer,
-    errorPage: errorPageReducer,
+    // newPassword: newPasswordReducer,
+    // recoverPassword: recoverPasswordReducer,
+    // testPage: testPageReducer,
+    // errorPage: errorPageReducer,
 
 })
 
-const store = createStore(rootReducer)
+export const store = createStore(rootReducer)
 
 export type AppRootStateType = ReturnType<typeof rootReducer>;
