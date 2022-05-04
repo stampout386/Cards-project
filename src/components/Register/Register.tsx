7031614bcf227dca2aaea3d1a1262fc951043f8f
madyler <madyler@mail.ru>
2022-05-04 21:04:39 +0300

    Register finaly

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -3,25 +3,38 @@ import SuperInputText from "../common/SuperInput/SuperInputText";
 import SuperButton from "../common/SuperButton/SuperButton";
 import './Register.css'
 import {cardsAPI} from "../../api/cardsAPI";
+import {useNavigate} from "react-router-dom";
 
 export function Register() {
     const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
     const [pass2, setPass2] = useState('')
-    const [error, setError] = useState(false)
+    const [error, setError] = useState('')
+
+    const navigate = useNavigate()
 
     const payload = {
         email: email,
         password: pass
     }
-    const onClickHandler = () => {
+    const onClickRegisterHandler = () => {
         if (pass === pass2) {
             cardsAPI.signUp(payload)
-                .then(r => {
-                    console.log(r)
+                .then(() => {
+                    navigate('/login')
+                })
+                .catch(r=>{
+                    setError(r.response.data.error)
                 })
         } else {
-            setError(true)
+            setError('ERROR:password must be the same')
+        }
+    };
+    const onClickCancelHandler = () => {
+        if(error){
+            setError('')
+        }else{
+            navigate('/login')
         }
     };
     return (
@@ -42,10 +55,10 @@ export function Register() {
                                 onChangeText={setPass2}
                                 value={pass2}/>
             </div>
-            {error && <div className={"error"}>ERROR:password must be the same </div>}
+            {error && <div className={"error"}>{error}</div>}
             <div>
-                <SuperButton className={'button'}>Cancel</SuperButton>
-                <SuperButton className={'button'} onClick={onClickHandler}>Register</SuperButton>
+                <SuperButton className={'button'} onClick={onClickCancelHandler}>Cancel</SuperButton>
+                <SuperButton className={'button'} onClick={onClickRegisterHandler}>Register</SuperButton>
             </div>
         </div>
     )
